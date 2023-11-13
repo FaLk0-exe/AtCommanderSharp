b03753895256a5ce0bcfd6e7a0af16a52ecceb3d
aprox512 <aprox512@gmail.com>
2023-11-13 21:59:01 +0200

    feat: ScheduledTaskData

---
+++ AtCommanderSharp/src/Data/Models/Tasks/AtScheduledTaskData.cs
@@ -0,0 +1,13 @@
+﻿namespace AtCommanderSharp.Data.Models.Tasks;
+
+public class AtScheduledTaskData
+{
+    private string _command;
+    private DateTime _executionDate;
+
+    public AtScheduledTaskData(string command, DateTime executionDate)
+    {
+        _command = command;
+        _executionDate = executionDate;
+    }
+}
